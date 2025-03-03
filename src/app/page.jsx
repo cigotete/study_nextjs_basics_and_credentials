b49bbe4a6aa40b62f2e01fcda5d053ef5619279c
Camilo Figueroa <cigotete@gmail.com>
2025-03-03 15:31:11 -0500

    Added some few clases to homepage.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="h-[calc(100vh-7rem)] flex justify-center items-center">
       Home Page
     </div>
   )
