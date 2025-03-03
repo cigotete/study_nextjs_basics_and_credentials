69fe3c18396d7be0907ea589b36b721c390154bd
Camilo Figueroa <cigotete@gmail.com>
2025-03-03 11:34:01 -0500

    Added authentication endpoint.

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import db from '@/libs/db'
+import bcrypt from 'bcrypt'
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials, req) {
+        console.log("credentials", credentials)
+
+        const userFound = await db.user.findUnique({
+            where: {
+                email: credentials.email
+            }
+        })
+
+        if (!userFound) throw new Error('No user found')
+
+        console.log("userFound", userFound)
+
+        const matchPassword = await bcrypt.compare(credentials.password, userFound.password)
+
+        if (!matchPassword) throw new Error('Wrong password')
+
+        return {
+            id: userFound.id,
+            name: userFound.username,
+            email: userFound.email,
+        }
+      },
+    }),
+  ]
+};
+
+const handlerAuth = NextAuth(authOptions);
+
+export { handlerAuth as GET, handlerAuth as POST };
