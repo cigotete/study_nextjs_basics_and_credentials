137fb5ee8b6c54542745315245953cf1f5c520f6
Camilo Figueroa <cigotete@gmail.com>
2025-03-03 00:45:22 -0500

    Updated register form to send a request to register a user.

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,25 +1,120 @@
 "use client";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 function RegisterPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const router = useRouter();
+
+  const onSubmit = handleSubmit(
+    async (data) => {
+    if (data.password !== data.confirmPassword) {
+      return alert("Passwords do not match");
+    }
+
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.ok) {
+      router.push("/auth/login");
+    }
+  });
+
+  console.log(errors);
 
   return (
-    <div>
-      <form>
+    <div className="h-[calc(100vh-7rem)] flex justify-center items-center">
+      <form onSubmit={onSubmit} className="w-1/4">
         <h1>Register</h1>
 
-        <label>Username:</label>
-        <input type="text"/>
+        <br />
+        <label htmlFor="username">
+          Username:
+        </label>
+        <input
+          type="text"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "Username is required",
+            },
+          })}
+         
+          placeholder="username"
+        />
 
-        <label>Email:</label>
-        <input type="email"/>
+        {errors.username && errors.username.message}
 
-        <label>Password: </label>
-        <input type="password"/>
+        <br />
+        <label htmlFor="email">
+          Email:
+        </label>
+        <input
+          type="email"
+          {...register("email", {
+            required: {
+              value: true,
+              message: "Email is required",
+            },
+          })}
+         
+          placeholder="user@email.com"
+        />
+        {errors.email && errors.email.message}
 
-        <label>Confirm Password:</label>
-        <input type="password"/>
+        <br />
+        <label htmlFor="password">
+          Password:
+        </label>
+        <input
+          type="password"
+          {...register("password", {
+            required: {
+              value: true,
+              message: "Password is required",
+            },
+          })}
+         
+          placeholder="********"
+        />
+        {errors.password && errors.password.message}
 
-        <button>Register</button>
+        <br />
+        <label
+          htmlFor="confirmPassword"
+        >
+          Confirm Password:
+        </label>
+        <input
+          type="password"
+          {...register("confirmPassword", {
+            required: {
+              value: true,
+              message: "Confirm Password is required",
+            },
+          })}
+         
+          placeholder="********"
+        />
+        {errors.confirmPassword && errors.confirmPassword.message}
+
+        <br />
+        <button>
+          Register
+        </button>
       </form>
     </div>
   );
